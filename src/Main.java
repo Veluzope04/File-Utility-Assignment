2c736ccd4bf855ca444d106ba041966c99d3ab36
velu <veluzope776@gmail.com>
2023-09-12 21:28:21 +0530

    File Utility Assignment

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(FileUtil.size("C:\\Users\\velu\\OneDrive\\Desktop\\Velu")
+        );
+        System.out.println(FileUtil.count("Desktop\\Velu"));
+}
+}
